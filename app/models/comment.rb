c1c0fd2e63e1862f3cf3b9414bff1b7e9611e8f8
aron-helu <aronmilahela@gmail.com>
2022-10-26 20:29:28 +0300

    Fix linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    after_save :update_comments_counter
+  belongs_to :user
+  belongs_to :post
+  after_save :update_comments_counter
 
-    def update_comments_counter
-        post.increment!(:comments_counter)
-    end    
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :post, foreign_key: 'user_id'
-    has_many :comment, foreign_key: 'user_id'
-    has_many :like, foreign_key: 'user_id'
+  has_many :post, foreign_key: 'user_id'
+  has_many :comment, foreign_key: 'user_id'
+  has_many :like, foreign_key: 'user_id'
 
-    def recent_posts
-        post.order(created_at: :desc).limit(3)
-    end    
+  def recent_posts
+    post.order(created_at: :desc).limit(3)
+  end
 end
