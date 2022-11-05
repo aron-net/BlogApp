c6a6d0a89ec2a6da8eec23249f3c4a964b3d58e8
aron-helu <aronmilahela@gmail.com>
2022-11-05 17:25:32 +0300

    remove comment out code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do 
-  # get 'likes/create'
-  # get 'comments/new'
-  # get 'comments/create'
+
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #rails g controller users index show
