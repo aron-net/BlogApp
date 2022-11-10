e070ea3c6c62d9e33a4c62837871c21c9df66c5a
aron-helu <aronmilahela@gmail.com>
2022-11-10 20:14:29 +0300

    Create devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   add_flash_types :danger, :info, :warning, :success, :messages
-  def current_user
-    User.first
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
 end
