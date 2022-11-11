410674b674056ce50ef00e56038212914f6b2922
aron-helu <aronmilahela@gmail.com>
2022-11-12 00:56:35 +0300

    Add autorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def new
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
@@ -8,15 +9,34 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @user = User.find(params[:user_id])
-    comment = Comment.new(params.require(:comment).permit(:text))
+    comment = Comment.new(comment_params)
     comment.user = current_user
     comment.post = @post
 
-    if comment.save
-      redirect_to user_post_path(@user.id, @post.id)
-    else
-      flash[:notice] = "Error: Couldn't create comment"
-      render :new, locals: { comment: }
+    respond_to do |format|
+      format.html do
+        if comment.save
+          redirect_to user_post_path(@user.id, @post.id)
+        else
+          flash[:notice] = "Error: Couldn't create comment"
+          render :new, locals: {comment:}
+        end
+      end  
     end
   end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.post.decrement!(:comments_counter)
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_back_or_to user_posts_path(current_user), notice: 'Deleted!' }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end  
 end
