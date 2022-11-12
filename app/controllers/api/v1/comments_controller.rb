00b9ee0a9d7855930be5682ff82d608aac05a6f3
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-12 13:34:08 -0300

    Fix Linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,21 +1,21 @@
 module Api
-    module V1
-        class CommentsController < ApplicationController
-            before_action :authenticate_user!
-            def index
-                comments = Comment.where(user_id: params[:user_id])
-                render json: comments, status: :ok
-            end
+  module V1
+    class CommentsController < ApplicationController
+      before_action :authenticate_user!
+      def index
+        comments = Comment.where(user_id: params[:user_id])
+        render json: comments, status: :ok
+      end
 
-            def create
-                comment = Comment.new(text: params[:comment][:text], post_id:, user_id:)
+      def create
+        comment = Comment.new(text: params[:comment][:text], post_id:, user_id:)
 
-                if comment.save
-                    render json: comment, status: :ok
-                else
-                    render json: comment.errors, status: :unprocessable_entity
-                end
-            end
+        if comment.save
+          render json: comment, status: :ok
+        else
+          render json: comment.errors, status: :unprocessable_entity
         end
+      end
     end
+  end
 end
